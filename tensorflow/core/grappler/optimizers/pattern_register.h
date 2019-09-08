db4fc7c93718d3897fd713513483569e1acfface
wenqi.gwq <wenqi.gwq@alibaba-inc.com>
2019-09-08 17:24:50 +0800

    parallel gemm fusion struct

--- tensorflow/core/grappler/optimizers/pattern_register.h
+++ tensorflow/core/grappler/optimizers/pattern_register.h
@@ -7,11 +7,16 @@ namespace tensorflow {
 namespace grappler {
 
 REGISTER_FUSION_PATTERN(FusedGemm)
-    .FusionOpTypeString("Gemm")  // TODO Gemm
+    .FusionOpTypeString("Gemm")
     .BfsPatternNodes({{"BiasAdd", {0}},
                       {"Reshape", {0}},
                       {"MatMul", {}}})
     .SetFusionPatternImpl(new GemmFusionPatternImpl());
+
+REGISTER_FUSION_PATTERN(ParallelFusedGemm)
+    .FusionOpTypeString("ParallelGemm")
+    .BfsPatternNodes({{"Gemm", {}}})
+    .SetFusionPatternImpl(new ParallelGemmFusionPatternImpl());
 
 }  // namespace grappler
 }  // namespace tensorflow
