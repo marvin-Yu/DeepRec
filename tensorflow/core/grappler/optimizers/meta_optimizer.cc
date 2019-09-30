ded77eca2b640df3f0728f1ee6124972c5c5419e
zhujun <zhujun@taobao.com>
2019-09-30 21:50:51 +0800

    Fix compilation problems after The Merge

--- tensorflow/core/grappler/optimizers/meta_optimizer.cc
+++ tensorflow/core/grappler/optimizers/meta_optimizer.cc
@@ -32,10 +32,8 @@ limitations under the License.
 #include "tensorflow/core/grappler/optimizers/debug_stripper.h"
 #include "tensorflow/core/grappler/optimizers/dependency_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/function_optimizer.h"
-#include "tensorflow/core/grappler/optimizers/gemm_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/generic_layout_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/implementation_selector.h"
-#include "tensorflow/core/grappler/optimizers/kernel_fusion_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/loop_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/memory_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/model_pruner.h"
@@ -154,8 +152,6 @@ std::unique_ptr<GraphOptimizer> MetaOptimizer::MakeNewOptimizer(
                                       cfg_.scoped_allocator_opts()));
   MK_OPT("pin_to_host",
          new PinToHostOptimizer(cfg_.pin_to_host_optimization()));
-  MK_OPT("gemm", new GemmOptimizer());
-  // MK_OPT("kernel_fusion", new KernelFusionOptimizer());
 
   return std::unique_ptr<GraphOptimizer>();
 }
@@ -239,13 +235,6 @@ Status MetaOptimizer::InitializeOptimizers(
     optimizers->push_back(MakeUnique<ScopedAllocatorOptimizer>(
         cfg_.scoped_allocator_optimization(), cfg_.scoped_allocator_opts()));
   }
-  // if (cfg_.kernel_fusion_optimization() != RewriterConfig::OFF) {
-  //   optimizers->push_back(MakeUnique<KernelFusionOptimizer>());
-  // }
-  if (cfg_.gemm_optimization() != RewriterConfig::OFF) {
-    optimizers->push_back(MakeUnique<GemmOptimizer>());
-  }
-
   return InitializeCustomGraphOptimizers(std::set<string>(), optimizers);
 }
 
@@ -827,8 +816,7 @@ bool MetaOptimizerEnabled(const ConfigProto& cfg) {
          rewrite_cfg.pin_to_host_optimization() == RewriterConfig::ON ||
          AutoMixedPrecisionEnabled(rewrite_cfg.auto_mixed_precision()) ||
          !rewrite_cfg.optimizers().empty() ||
-         !rewrite_cfg.custom_optimizers().empty() ||
-         rewrite_cfg.kernel_fusion_optimization() != RewriterConfig::OFF;
+         !rewrite_cfg.custom_optimizers().empty();
 }
 
 Status RunMetaOptimizer(const GrapplerItem& item, const ConfigProto& cfg,
