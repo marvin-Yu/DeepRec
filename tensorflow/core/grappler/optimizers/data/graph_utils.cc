35826a7634ba908ed027d948135c41ed1eea5647
Yunlong Xu <yunlong.xyl@alibaba-inc.com>
2019-12-17 17:52:15 +0800

    Downgrade protobuf to be consistent with the version used in pai-tf-1.12.

--- tensorflow/core/grappler/optimizers/data/graph_utils.cc
+++ tensorflow/core/grappler/optimizers/data/graph_utils.cc
@@ -284,7 +284,7 @@ Status GetDatasetOutputTypesAttr(const NodeDef& node,
                                  DataTypeVector* output_types) {
   // We don't name the output_types attr consistently, so should check for both.
   for (const string& attr_name : {"output_types", "Toutput_types"}) {
-    if (node.attr().contains(attr_name)) {
+    if (node.attr().find(attr_name) != node.attr().end()) {
       return GetNodeAttr(node, attr_name, output_types);
     }
   }
