54c2851ae9d810d682904301f98383748cf34640
jingshan.ljs <jingshan.ljs@alibaba-inc.com>
2020-02-21 16:40:28 +0800

    revert executor

--- tensorflow/core/common_runtime/executor.cc
+++ tensorflow/core/common_runtime/executor.cc
@@ -21,7 +21,6 @@ limitations under the License.
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <pthread.h>
 
 #include "absl/memory/memory.h"
 #include "absl/strings/string_view.h"
@@ -51,7 +50,6 @@ limitations under the License.
 #include "tensorflow/core/lib/core/notification.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/lib/core/stringpiece.h"
-#include "tensorflow/core/lib/core/spin_lock.h"
 #include "tensorflow/core/lib/core/threadpool.h"
 #include "tensorflow/core/lib/gtl/flatmap.h"
 #include "tensorflow/core/lib/gtl/flatset.h"
@@ -185,8 +183,6 @@ struct NodeItem {
   size_t num_output_edges;
 
   PendingCounts::Handle pending_id;
-
-  mutable spin_lock node_lock;
 
   const EdgeInfo* output_edge_list() const { return output_edge_base(); }
 
@@ -809,8 +805,8 @@ Status InferAllocAttr(const Node* n, const Node* dst,
       // Value is going to be the sink of an RPC.
       attr->set_nic_compatible(true);
       VLOG(2) << "node " << n->name() << " is the sink of an RPC in";
-    } else if ((local_dev_name.type == "CPU" || n->IsHostRecv() ||
-                n->IsHostFuseRecv()) && parsed_src_name.type != "CPU") {
+    } else if ((local_dev_name.type == "CPU" || n->IsHostRecv()) &&
+               parsed_src_name.type != "CPU") {
       // Value is going to be the sink of a local DMA from GPU to CPU (or
       // other types of accelerators).
       attr->set_gpu_compatible(true);
@@ -975,8 +971,7 @@ class ExecutorState {
     Entry* input_tensors;
 
     // The number of outstanding ops for each iteration.
-    std::atomic_int_fast32_t outstanding_ops;
-//    size_t outstanding_ops;
+    size_t outstanding_ops;
 
     // The number of outstanding frames for each iteration.
     int outstanding_frame_count;
@@ -1143,8 +1138,8 @@ class ExecutorState {
                                               int64 iter, TaggedNodeSeq* ready)
         EXCLUSIVE_LOCKS_REQUIRED(mu) {
       IterationState* istate = GetIteration(iter);
-      int outstanding_ops = istate->outstanding_ops.fetch_sub(1);
-      if (outstanding_ops != 1) {
+      istate->outstanding_ops--;
+      if (istate->outstanding_ops != 0) {
         return false;
       } else {
         return CleanupIterations(gview, iter, ready);
@@ -1236,7 +1231,6 @@ class ExecutorState {
         front_index_ = 0;
       }
     }
-    size_t size() const {return ready_.size(); }
     bool empty() const { return ready_.empty(); }
     const TaggedNode* begin() const { return ready_.begin() + front_index_; }
     const TaggedNode* end() const { return ready_.end(); }
@@ -1254,17 +1248,14 @@ class ExecutorState {
   const bool log_memory_;
 
   int64 step_id_;
-  int64 round_step_id_;
   // Not owned.
   Rendezvous* rendezvous_;
-  Rendezvous* global_rendezvous_;
   Executor::RendezvousFactory* create_rendezvous_ = nullptr;
   CollectiveExecutor* collective_executor_ = nullptr;
   SessionState* session_state_;
   string session_handle_;
   const SessionMetadata* session_metadata_ = nullptr;
   TensorStore* tensor_store_;
-  bool own_tensor_store_;
   // Step-local container.
   ScopedStepContainer* step_container_;
   StepStatsCollectorInterface* const stats_collector_;
@@ -1395,9 +1386,7 @@ ExecutorState::ExecutorState(const Executor::Args& args, ExecutorImpl* impl)
     : vlog_(VLOG_IS_ON(1)),
       log_memory_(LogMemory::IsEnabled()),
       step_id_(args.step_id),
-      round_step_id_(args.round_step_id),
       rendezvous_(args.rendezvous),
-      global_rendezvous_(args.global_rendezvous),
       create_rendezvous_(&impl->params_.rendezvous_factory),
       collective_executor_(args.collective_executor),
       session_state_(args.session_state),
@@ -1435,12 +1424,6 @@ ExecutorState::ExecutorState(const Executor::Args& args, ExecutorImpl* impl)
       root_frame_->pending_counts, root_frame_->total_input_tensors);
 
   outstanding_frames_.insert({root_frame_->frame_name, root_frame_});
-  if (!tensor_store_) {
-    tensor_store_ = new TensorStore;
-    own_tensor_store_ = true;
-  } else {
-    own_tensor_store_ = false;
-  }
 }
 
 ExecutorState::~ExecutorState() {
@@ -1451,9 +1434,6 @@ ExecutorState::~ExecutorState() {
     it->Unref();
   }
   delete slice_reader_cache_;
-  if (own_tensor_store_) {
-    delete tensor_store_;
-  }
 }
 
 Status ExecutorImpl::BuildControlFlowInfo(const Graph* g,
@@ -1649,7 +1629,6 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_nsec) {
 
   OpKernelContext::Params params;
   params.step_id = step_id_;
-  params.round_step_id = round_step_id_;
   // Override device's threadpool if user provides an intra_op_threadpool
   Device* device = impl_->params_.device;
   if (user_device_) {
@@ -1660,7 +1639,6 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_nsec) {
   params.log_memory = log_memory_;
   params.record_tensor_accesses = impl_->device_record_tensor_accesses_;
   params.rendezvous = rendezvous_;
-  params.global_rendezvous = global_rendezvous_;
   params.create_rendezvous = create_rendezvous_;
   params.collective_executor = collective_executor_;
   params.session_state = session_state_;
@@ -2058,7 +2036,7 @@ Status ExecutorState::ProcessOutputs(const NodeItem& item, OpKernelContext* ctx,
     if (val.tensor == nullptr) {
       // Unless it's a Switch or a Recv, the node must produce a
       // tensor value at i-th output.
-      if (!IsSwitch(node) && !IsRecv(node) && !IsFuseRecv(node)) {
+      if (!IsSwitch(node) && !IsRecv(node)) {
         s.Update(errors::Internal("Missing ", i, "-th output from ",
                                   FormatNodeForError(*node)));
       }
@@ -2147,9 +2125,9 @@ void ExecutorState::PropagateOutputs(const TaggedNode& tagged_node,
     // Fast path for nodes types that don't need special handling
     DCHECK_EQ(input_frame, output_frame);
     // Normal path for most nodes
-//    mutex_lock l(input_frame->mu);
+    mutex_lock l(input_frame->mu);
     output_frame->ActivateNodes(item, is_dead, output_iter, outputs, ready);
-    is_frame_done = input_frame->DecrementOutstandingOps(
+    is_frame_done = input_frame->DecrementOutstandingOpsLocked(
         &impl_->gview_, input_iter, ready);
   } else if (item->is_enter) {
     FindOrCreateChildFrame(input_frame, input_iter, node, &output_frame);
@@ -2635,9 +2613,9 @@ void ExecutorState::DeleteFrame(FrameState* frame, TaggedNodeSeq* ready) {
       for (const Edge* e : node->out_edges()) {
         const Node* dst_node = e->dst();
 
-        const NodeItem* dst_item = impl_->gview_.node(dst_node->id());
-        const auto dst_pending_id = dst_item->pending_id;
-        auto &node_lock = dst_item->node_lock;
+        const auto dst_pending_id =
+            impl_->gview_.node(dst_node->id())->pending_id;
+
         // TODO(yuanbyu): We don't need this if we require the subgraph
         // given to an executor not to contain a sink node.
         if (dst_node->IsSink()) continue;
@@ -2647,14 +2625,12 @@ void ExecutorState::DeleteFrame(FrameState* frame, TaggedNodeSeq* ready) {
         // We know this is a dead input to dst.
         if (IsMerge(dst_node)) {
           if (e->IsControlEdge()) {
-            std::lock_guard<spin_lock> l(node_lock);
             parent_iter_state->decrement_pending(dst_pending_id, 2);
             int count = parent_iter_state->pending(dst_pending_id);
             int dead_cnt = parent_iter_state->dead_count(dst_pending_id);
             dst_dead = (dead_cnt == dst_node->num_inputs());
             dst_ready = (count == 0) || ((count == 1) && dst_dead);
           } else {
-            std::lock_guard<spin_lock> l(node_lock);
             parent_iter_state->increment_dead_count(dst_pending_id);
             const int dead_cnt = parent_iter_state->dead_count(dst_pending_id);
             dst_dead = (dead_cnt == dst_node->num_inputs());
@@ -2662,7 +2638,6 @@ void ExecutorState::DeleteFrame(FrameState* frame, TaggedNodeSeq* ready) {
                 (parent_iter_state->pending(dst_pending_id) == 1) && dst_dead;
           }
         } else {
-          std::lock_guard<spin_lock> l(node_lock);
           parent_iter_state->increment_dead_count(dst_pending_id);
           dst_ready =
               (parent_iter_state->decrement_pending(dst_pending_id, 1) == 0);
@@ -2721,7 +2696,6 @@ void ExecutorState::FrameState::ActivateNodes(const NodeItem* item,
     const NodeItem* dst_item = gview.node(dst_id);
     const PendingCounts::Handle dst_pending_id = dst_item->pending_id;
     const int src_slot = e.output_slot;
-    auto &node_lock = dst_item->node_lock;
 
     // TODO(yuanbyu): We don't need this if we require the subgraph
     // given to an executor not to contain a sink node.
@@ -2734,7 +2708,6 @@ void ExecutorState::FrameState::ActivateNodes(const NodeItem* item,
     // analysis happy.
     const bool is_control_edge = (src_slot == Graph::kControlSlot);
     bool dst_need_input = !is_control_edge;
-    std::lock_guard<spin_lock> l(node_lock);
     if (dst_item->is_merge) {
       // A merge node is ready if all control inputs have arrived and either
       // a) a live data input becomes available or b) all data inputs are
@@ -2846,7 +2819,7 @@ void ExecutorState::FrameState::AddLoopInv(const NodeItem* item,
 
 bool ExecutorState::FrameState::IsIterationDone(int64 iter) {
   IterationState* iter_state = GetIteration(iter);
-  if (iter_state->outstanding_ops.load() == 0 &&
+  if (iter_state->outstanding_ops == 0 &&
       iter_state->outstanding_frame_count == 0) {
     if (iter == 0) {
       // The enclosing frame has no pending input.
