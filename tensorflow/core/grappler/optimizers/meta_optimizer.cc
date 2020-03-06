1e0b648d5d6a0929fcaad6375e644790cd711233
A. Unique TensorFlower <gardener@tensorflow.org>
2020-03-07 04:35:04 +0800

    [Grappler] Split common subgraph elimination out into a separate Grappler optimizer. This allows us to run it before constant folding and function optimizer to reduce the size of the graph earlier and save time spent in grappler. In particular, this reduces the size of the graph for which we run static shape inference, which is very expensive.
    Change NodeMap to use absl::node_hash_map instead of std::unordered_map.
    
    On a particular model with 215k nodes and 255k edges, I measure a 25% speedup of the Grappler MetaOptimizer overall.
    
    PiperOrigin-RevId: 299413265
    Change-Id: I741e8f22fa8169044d3d51b81e08d20df301d506

--- tensorflow/core/grappler/optimizers/meta_optimizer.cc
+++ tensorflow/core/grappler/optimizers/meta_optimizer.cc
@@ -27,6 +27,7 @@ limitations under the License.
 #include "tensorflow/core/grappler/optimizers/arithmetic_optimizer.h"
 #include "tensorflow/core/grappler/optimizers/auto_mixed_precision.h"
 #include "tensorflow/core/grappler/optimizers/auto_parallel.h"
+#include "tensorflow/core/grappler/optimizers/common_subgraph_elimination.h"
 #include "tensorflow/core/grappler/optimizers/constant_folding.h"
 #include "tensorflow/core/grappler/optimizers/custom_graph_optimizer_registry.h"
 #include "tensorflow/core/grappler/optimizers/debug_stripper.h"
@@ -208,6 +209,8 @@ std::unique_ptr<GraphOptimizer> MetaOptimizer::MakeNewOptimizer(
            new AutoMixedPrecision(AutoMixedPrecisionMode::BF16));
   }
   MK_OPT("memory", new MemoryOptimizer(RewriterConfig::MANUAL));
+  MK_OPT("common_subgraph_elimination",
+         new CommonSubgraphElimination(cfg_.common_subgraph_elimination()));
   MK_OPT("arithmetic", new ArithmeticOptimizer(cfg_.arithmetic_optimization()));
   MK_OPT("autoparallel", new AutoParallel(cfg_.auto_parallel().num_replicas()));
   MK_OPT("loop", new LoopOptimizer(cfg_.loop_optimization(), cpu_device_));
@@ -254,6 +257,11 @@ Status MetaOptimizer::InitializeOptimizers(
   if (cfg_.fuse_tile_concat_matmul() != RewriterConfig::OFF) {
     optimizers->push_back(
         std::unique_ptr<GraphOptimizer>(new UnfuseBatchNormOptimizer(cpu_device_)));
+  }
+  if (cfg_.common_subgraph_elimination() != RewriterConfig::OFF &&
+      cfg_.arithmetic_optimization() != RewriterConfig::OFF) {
+    optimizers->push_back(MakeUnique<CommonSubgraphElimination>(
+        cfg_.common_subgraph_elimination()));
   }
   if (cfg_.debug_stripper() == RewriterConfig::ON) {
     optimizers->push_back(MakeUnique<DebugStripper>());
@@ -980,6 +988,7 @@ bool MetaOptimizerEnabled(const ConfigProto& cfg) {
          rewrite_cfg.constant_folding() != RewriterConfig::OFF ||
          rewrite_cfg.shape_optimization() != RewriterConfig::OFF ||
          rewrite_cfg.remapping() != RewriterConfig::OFF ||
+         rewrite_cfg.common_subgraph_elimination() != RewriterConfig::OFF ||
          rewrite_cfg.arithmetic_optimization() != RewriterConfig::OFF ||
          rewrite_cfg.loop_optimization() != RewriterConfig::OFF ||
          rewrite_cfg.dependency_optimization() != RewriterConfig::OFF ||
