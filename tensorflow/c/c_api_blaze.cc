829ab04716c65c8959cce38be586c6e4650fed1e
wenqi.gwq <wenqi.gwq@alibaba-inc.com>
2020-05-25 20:12:15 +0800

    update

--- tensorflow/c/c_api_blaze.cc
+++ tensorflow/c/c_api_blaze.cc
@@ -80,6 +80,10 @@ limitations under the License.
 #include "tensorflow/core/public/version.h"
 #include "tensorflow/core/util/env_var.h"
 
+// TODO USE CUDA
+#include "tensorflow/core/common_runtime/gpu/gpu_device.h"
+#include "tensorflow/core/public/session_options.h"
+
 
 // The implementation below is at the top level instead of the
 // brain namespace because we are defining 'extern "C"' functions.
@@ -292,7 +296,8 @@ void TF_GraphSetDevice(TF_Graph* graph, int cpu_id, int gpu_id) {
   mutex_lock l(graph->mu);
   Graph* g = &(graph->graph);
 
-  AutoPlaceNodesOnCPU(g);
+  // TODO
+//  AutoPlaceNodesOnCPU(g);
 
   LOG(INFO) << "TF_GraphSetDevice: cpu_id, gpu_id = "
             << cpu_id << ", " << gpu_id;
@@ -629,6 +634,7 @@ void TF_SessionMakeCallable(TF_Session* tf_sess, TF_CallableHandle* callable_han
     opts.mutable_fetch_devices()->insert({fetch_name, device_name});
   }
   opts.set_fetch_skip_sync(true);
+  LOG(INFO) << opts.DebugString();
   Session::CallableHandle handle;
   status->status = tf_sess->session->MakeCallable(opts, &handle);
   if (TF_GetCode(status) != TF_OK) {
@@ -670,36 +676,25 @@ void TF_CudaMemAlloc(int virtual_gpu_id, void** gpu_ptr, size_t length) {
   Status s = GpuIdManager::TfToPlatformGpuId(tf_gpu_id, &platform_gpu_id);
   stream_executor::StreamExecutor* se =
       GpuIdUtil::ExecutorForPlatformGpuId(platform_gpu_id).ValueOrDie();
-  *gpu_ptr = se->UnifiedMemoryAllocate(length);
+  static tensorflow::GPUOptions gpu_options;
+  tensorflow::BaseGPUDevice::StreamGroup* stream_group = tensorflow::StreamGroupFactory::Global().GetOrCreate(
+      tf_gpu_id, 0, se, gpu_options);
+  *gpu_ptr = stream_group->compute->parent()->UnifiedMemoryAllocate(length);
 }
 
-// TODO USE CUDA
-#include "tensorflow/core/common_runtime/gpu/gpu_device.h"
-#include "tensorflow/core/public/session_options.h"
 
 void TF_CudaMemCopyHostToDeviceAsync(int virtual_gpu_id, void* device_ptr, const void* host_ptr, size_t length) {
-  /*
-  TfGpuId tf_gpu_id(virtual_gpu_id);
-  PlatformGpuId platform_gpu_id;
-  Status s = GpuIdManager::TfToPlatformGpuId(tf_gpu_id, &platform_gpu_id);
-  stream_executor::StreamExecutor* executor =
-      GpuIdUtil::ExecutorForPlatformGpuId(platform_gpu_id).ValueOrDie();
-  stream_executor::Stream stream(executor);
-  stream.Init();
-  stream_executor::DeviceMemoryBase device_memory(device_ptr, length);
-  stream.ThenMemcpy(&device_memory, host_ptr, length);*/
-
-
   static tensorflow::GPUOptions gpu_options;
   TfGpuId tf_gpu_id(virtual_gpu_id);
   PlatformGpuId platform_gpu_id;
   Status s = GpuIdManager::TfToPlatformGpuId(tf_gpu_id, &platform_gpu_id);
   stream_executor::StreamExecutor* executor =
       GpuIdUtil::ExecutorForPlatformGpuId(platform_gpu_id).ValueOrDie();
-  tensorflow::BaseGPUDevice::StreamGroup* stream_group = tensorflow::BaseGPUDevice::StreamGroupFactory::Global().GetOrCreate(
+  tensorflow::BaseGPUDevice::StreamGroup* stream_group = tensorflow::StreamGroupFactory::Global().GetOrCreate(
       tf_gpu_id, 0, executor, gpu_options);
   stream_executor::DeviceMemoryBase device_memory(device_ptr, length);
   stream_group->compute->ThenMemcpy(&device_memory, host_ptr, length);
 }
 
 }  // end extern "C"
+
--- tensorflow/core/common_runtime/gpu/gpu_device.cc
+++ tensorflow/core/common_runtime/gpu/gpu_device.cc
@@ -225,77 +225,6 @@ class EigenGpuStreamDevice : public ::Eigen::StreamInterface {
   TF_DISALLOW_COPY_AND_ASSIGN(EigenGpuStreamDevice);
 };
 
-// This factory helps to ensure that different GPU device objects that refer to
-// the same physical device and stream group id use the same stream group
-// object (and therefore the same CUDA streams). This is necessary since there
-// is a single memory allocator per device (see ProcessState::GetGPUAllocator)
-// and allocators must not be shared across streams.
-class BaseGPUDevice::StreamGroupFactory {
- public:
-  // Returns the unique stream group for use with the stream defined by
-  // {tf_gpu_id, stream_group_within_gpu}, creating it if it does not yet
-  // exist.
-  // This function is thread safe.
-  BaseGPUDevice::StreamGroup* GetOrCreate(TfGpuId tf_gpu_id,
-                                          int stream_group_within_gpu,
-                                          se::StreamExecutor* executor,
-                                          const GPUOptions& options) {
-    mutex_lock guard(lock_);
-    StreamGroup* group =
-        &streams_[key_type(tf_gpu_id.value(), stream_group_within_gpu)];
-    if (!group->compute) {
-      group->compute = new se::Stream(executor);
-      group->compute->Init();
-      VLOG(2) << "Created stream[" << stream_group_within_gpu
-              << "] = " << group->compute;
-
-      group->host_to_device = new se::Stream(executor);
-      group->host_to_device->Init();
-      VLOG(2) << "Created host_to_device_stream[" << stream_group_within_gpu
-              << "] = " << group->host_to_device;
-
-      group->device_to_host = new se::Stream(executor);
-      group->device_to_host->Init();
-      VLOG(2) << "Created device_to_host_stream[" << stream_group_within_gpu
-              << "] = " << group->device_to_host;
-
-      int num_d2d_streams =
-          options.experimental().num_dev_to_dev_copy_streams();
-      if (num_d2d_streams == 0) num_d2d_streams = 1;
-      if (num_d2d_streams < 1 || num_d2d_streams > 4) {
-        LOG(ERROR)
-            << "Illegal GPUOptions.experimental.num_dev_to_dev_copy_streams="
-            << num_d2d_streams << " set to 1 instead.";
-        num_d2d_streams = 1;
-      }
-      for (int i = 0; i < num_d2d_streams; ++i) {
-        se::Stream* stream = new se::Stream(executor);
-        stream->Init();
-        group->device_to_device.push_back(stream);
-        VLOG(2) << "Created device_to_device_stream[" << stream_group_within_gpu
-                << "] = " << group->device_to_device.back();
-      }
-    }
-    return group;
-  }
-
-  // Returns a reference to the StreamGroupFactory singleton. Note that this is
-  // never destroyed, so the objects it owns are never deleted.
-  static StreamGroupFactory& Global() {
-    static StreamGroupFactory* instance = new StreamGroupFactory();
-    return *instance;
-  }
-
- private:
-  mutex lock_;
-  using key_type = std::tuple<int, int>;
-  std::map<key_type, StreamGroup> streams_;
-
-  // StreamGroupFactory cannot be created directly; Call
-  // StreamGroupFactory::Global() to get the global instance.
-  StreamGroupFactory() = default;
-  TF_DISALLOW_COPY_AND_ASSIGN(StreamGroupFactory);
-};
 
 BaseGPUDevice::BaseGPUDevice(const SessionOptions& options, const string& name,
                              Bytes memory_limit, const DeviceLocality& locality,
@@ -371,7 +300,7 @@ Status BaseGPUDevice::Init(const SessionOptions& options) {
 
   // Create the specified number of GPU streams
   for (int i = 0; i < get_max_streams(); i++) {
-    streams_.push_back(StreamGroupFactory::Global().GetOrCreate(
+    streams_.push_back(tensorflow::StreamGroupFactory::Global().GetOrCreate(
         tf_gpu_id_, i, executor_, options.config.gpu_options()));
     device_contexts_.push_back(new GPUDeviceContext(
         i, streams_.back()->compute, streams_.back()->host_to_device,
