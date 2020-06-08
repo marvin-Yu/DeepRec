56bb9f644f1076d14f535adb42d40796959b0348
wenqi.gwq <wenqi.gwq@alibaba-inc.com>
2020-06-08 20:33:02 +0800

    change copy result mode async to sync

--- tensorflow/c/c_api_blaze.cc
+++ tensorflow/c/c_api_blaze.cc
@@ -764,9 +764,10 @@ bool TF_CudaMemCopyDeviceToHost(int virtual_gpu_id, void* host_ptr, const void* 
   }
   stream_executor::DeviceMemoryBase device_memory(const_cast<void*>(device_ptr), length);
   // sync 
-  //stream->parent()->SynchronousMemcpyD2H(device_memory, length, host_ptr);
+  stream->parent()->SynchronousMemcpyD2H(device_memory, length, host_ptr);
 
   // async
+  /*
   stream->ThenMemcpy(host_ptr, device_memory, length);
   auto event = std::make_shared<stream_executor::Event>(stream->parent());
   if (!event->Init()) {
@@ -774,7 +775,7 @@ bool TF_CudaMemCopyDeviceToHost(int virtual_gpu_id, void* host_ptr, const void* 
     return false;
   }
   stream->ThenRecordEvent(event.get());
-  stream->ThenWaitFor(event.get());
+  stream->ThenWaitFor(event.get());*/
   return true;
 }
 
