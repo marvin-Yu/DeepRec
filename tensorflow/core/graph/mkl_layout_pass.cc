187da5b169d82ed86c4219f9899c977d41b045a5
Varghese, Jojimon <jojimon.varghese@intel.com>
2020-09-25 14:37:05 -0700

    PR #43573: Fix for concatv2 unit test in [INTEL MKL] enabled Tensorflow

--- tensorflow/core/graph/mkl_layout_pass.cc
+++ tensorflow/core/graph/mkl_layout_pass.cc
@@ -422,7 +422,7 @@ class MklLayoutRewritePass : public GraphOptimizationPass {
                       CopyAttrsAll, AlwaysRewrite, GetRewriteCause()});
     rinfo_.push_back({csinfo_.concatv2,
                       mkl_op_registry::GetMklOpName(csinfo_.concatv2),
-                      CopyAttrsAll, AlwaysRewrite, GetRewriteCause()});
+                      CopyAttrsAll, ConcatV2Rewrite, GetRewriteCause()});
     rinfo_.push_back(
         {csinfo_.conjugate_transpose,
          mkl_op_registry::GetMklOpName(csinfo_.conjugate_transpose),
@@ -576,7 +576,7 @@ class MklLayoutRewritePass : public GraphOptimizationPass {
                       CopyAttrsAll, AlwaysRewrite, GetRewriteCause()});
     rinfo_.push_back({csinfo_.quantized_concatv2,
                       mkl_op_registry::GetMklOpName(csinfo_.quantized_concatv2),
-                      CopyAttrsAll, AlwaysRewrite, GetRewriteCause()});
+                      CopyAttrsAll, ConcatV2Rewrite, GetRewriteCause()});
     rinfo_.push_back({csinfo_.quantized_conv2d,
                       mkl_op_registry::GetMklOpName(csinfo_.quantized_conv2d),
                       CopyAttrsQuantizedConv2D, AlwaysRewrite,
@@ -1531,6 +1531,12 @@ class MklLayoutRewritePass : public GraphOptimizationPass {
       return true;
     }
     return false;
+  }
+  // For oneDNN, only int32 is supported for axis data type
+  static bool ConcatV2Rewrite(const Node *n) {
+    DataType T;
+    GetNodeAttr(n->def(),"Tidx", &T);
+    return (T == DT_INT32);
   }
 
   static bool DequantizeRewrite(const Node* n) {
