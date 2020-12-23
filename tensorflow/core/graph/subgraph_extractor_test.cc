076014805d5b25bebc5cce15df79d49292317153
zexin.yzx <zexin.yzx@alibaba-inc.com>
2020-12-23 14:52:36 +0800

    fix ut coredump

--- tensorflow/core/graph/subgraph_extractor_test.cc
+++ tensorflow/core/graph/subgraph_extractor_test.cc
@@ -10,11 +10,13 @@
 
 #include <string>
 #include "tensorflow/core/graph/graph_constructor.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/lib/io/path.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/test.h"
 #include "tensorflow/core/platform/protobuf.h"
 
-const static std::string SIMPLE_MODE_PATH = "graph/testdata/simple_model_test.pbtxt";
+const static std::string SIMPLE_MODE_PATH = "core/graph/testdata/simple_model_test.pbtxt";
 
 namespace tensorflow {
 namespace {
@@ -25,15 +27,16 @@ void ReadFileToStringOrDie(Env* env, const string& filename, string* output) {
 
 class SubgraphExtractorTest : public ::testing::Test {
  protected:
-  SubgraphExtractorTest() {};
-  void Setup(const std::string& graph_path) override {
+  SubgraphExtractorTest() : 
+      graph_(OpRegistry::Global()) {};
+  void Setup(const std::string& graph_path) {
     std::string proto_string;
     std::string filename =
         io::JoinPath(testing::TensorFlowSrcRoot(), graph_path);
     ReadFileToStringOrDie(Env::Default(), filename, &proto_string);
     protobuf::TextFormat::ParseFromString(proto_string, &graph_def_);
     
-    TF_CHECK_OK(ConvertNodeDefsToGraph({}, graph_def_, &graph_));
+    TF_CHECK_OK(ConvertGraphDefToGraph({}, graph_def_, &graph_));
     LOG(INFO) << "Setup finish";
   }
 
@@ -41,7 +44,7 @@ class SubgraphExtractorTest : public ::testing::Test {
 
   GraphDef graph_def_;
   Graph graph_;
-}
+};
 
 TEST_F(SubgraphExtractorTest, ExtractSubgraphSimple) {
   Setup(SIMPLE_MODE_PATH);
