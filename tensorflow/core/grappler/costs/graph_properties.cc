7ade9182af6fa47e754c28dd2923e2be90e52596
jingshan.ljs <jingshan.ljs@alibaba-inc.com>
2021-06-08 10:54:32 +0800

    npu bug fix revert lib_ops fix this

--- tensorflow/core/grappler/costs/graph_properties.cc
+++ tensorflow/core/grappler/costs/graph_properties.cc
@@ -1662,9 +1662,6 @@ class SymbolicShapeRefiner {
   }
 
   Status InferShapes(const NodeDef& node, NodeContext* c) {
-    if (node.op() == "HgEngine" || node.op() == "HgDequant") {
-      return Status::OK();
-    }
     // Infer the shapes of output tensors.
     if (!c->op_data || c->op_data->shape_inference_fn == nullptr ||
         !c->inference_context->Run(c->op_data->shape_inference_fn).ok()) {
