caefe326084a31cbc2cbb31107ddd9b6858daa51
jingshan.ljs <jingshan.ljs@alibaba-inc.com>
2021-08-13 15:39:28 +0800

    cp with -d

--- install11.sh
+++ install11.sh
@@ -2,9 +2,10 @@ export TEST_TMPDIR=
 EXTERNAL_DIR=$(dirname `readlink -f $0`)/../_external
 #prepare cuda-11.2
 if [ ! -d $EXTERNAL_DIR/usr/local/cuda-11.2  ]; then
+    rm -rf cuda-11.2*
     wget http://109717.oss-cn-hangzhou-zmf.aliyuncs.com/cuda-11.2.tar.gz -O cuda-11.2.tar.gz
     tar -zxvf cuda-11.2.tar.gz
-    cp -r cuda-11.2 $EXTERNAL_DIR/usr/local/
+    cp -r -d cuda-11.2 $EXTERNAL_DIR/usr/local/
 fi
 #python ./configure.py
 export TF_CUDA_PATHS=$EXTERNAL_DIR/usr/local/cuda-11.2/
