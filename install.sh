dca387207bc2a3e4580b4b031161576eae2dfb3d
jingshan.ljs <jingshan.ljs@alibaba-inc.com>
2021-09-07 14:49:16 +0800

    rm blank

--- install.sh
+++ install.sh
@@ -42,10 +42,10 @@ declare -a install_targets=("tensorflow/libtensorflow_framework.so"
                             "tensorflow/core/libtestlib.so"
                             "tensorflow/core/kernels/libops_testutil.so"
 )
-if [ -f ".tf_configure.bazelrc.cuda10" ]; then    
-    cp .tf_configure.bazelrc.cuda10 .tf_configure.bazelrc   
-else    
-    python ./configure.py   
+if [ -f ".tf_configure.bazelrc.cuda10" ]; then
+    cp .tf_configure.bazelrc.cuda10 .tf_configure.bazelrc
+else
+    python ./configure.py
 fi
 #if [ ! -f ".tf_configure.bazelrc" ]; then
 #    python ./configure.py
