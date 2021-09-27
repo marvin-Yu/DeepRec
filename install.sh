90c9da016caf04edb70aa7ed385d8fe86aaf20b3
jingshan.ljs <jingshan.ljs@alibaba-inc.com>
2021-09-28 00:34:20 +0800

    open -g mode

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ fi
 ## now loop through the above array
 for target in "${targets[@]}"
 do
-    bazel build --copt=-mavx2 --config=cuda --copt -mfpmath=both --copt -mfma --copt -msse4.2 --copt -D_GLIBCXX_USE_CXX11_ABI=0 --copt -DGOOGLE_CUDA=1  $target
+    bazel build -c opt --copt -g --copt=-mavx2 --config=cuda --copt -mfpmath=both --copt -mfma --copt -msse4.2 --copt -D_GLIBCXX_USE_CXX11_ABI=0 --copt -DGOOGLE_CUDA=1  $target
 done
 
 EXTERNAL_DIR="../_external"
